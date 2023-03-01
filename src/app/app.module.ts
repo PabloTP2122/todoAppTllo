8dd96d232f4c9c9f577025d44f8c386b58e3c12d
Pablo Torres <a01630637@itesm.mx>
2023-02-28 20:13:07 -0600

    Commit inicial, interfaz maquetada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { BtnComponent } from './components/btn/btn.component';
+import { BoardsComponent } from './pages/boards/boards.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BoardComponent } from './pages/board/board.component';
+
+//CDK
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+//Forms
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BtnComponent,
+    BoardsComponent,
+    NavbarComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    OverlayModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    CdkAccordionModule,
+    DragDropModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
